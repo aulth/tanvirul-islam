a828dcdbb451d69894d527b77538c0962391c485
aulth <mohdusman.you@gmail.com>
2023-08-15 23:16:01 +0530

    profile input tag functionality added

--- src/components/user/Profile.jsx
+++ src/components/user/Profile.jsx
@@ -1,12 +1,30 @@
 import Footer from "@/components/Footer";
-import React, { useEffect, useContext } from "react";
+import React, { useState, useContext } from "react";
 import Image from "next/image";
 import { BiMenu } from "react-icons/bi";
-import { MdCancel, MdCheck, MdEdit} from "react-icons/md";
+import { MdCancel, MdCheck, MdEdit, MdFacebook, MdWhatsapp } from "react-icons/md";
 import Sidebar from "./Sidebar";
 import { ContextData } from "@/context/context";
+import { BsTwitter, BsWhatsapp } from "react-icons/bs";
+import { AiOutlineTwitter } from "react-icons/ai";
 const Profile = () => {
-  const {toggleProfileMenu} = useContext(ContextData);
+  const { toggleProfileMenu } = useContext(ContextData);
+  const [data, setData] = useState({ name: "Mohd Usman", username: "mohd.usman_7d3", bio: "Hi, I am Mohd Usman I am pursuing B.Tech Electrical Engineering from Jamia Millia Islamia", title: "Fullstack Developer", course: "B.Tech (Electrical Engineering)", start: "2020", end: "2024" })
+  const [dataCopy, setDataCopy] = useState(data);
+  const [changeDetected, setChangeDetected] = useState(false);
+  const handleOnChange = (e) => {
+    if(!changeDetected){
+      setDataCopy(data);
+    }
+    setChangeDetected(true);
+    e.preventDefault();
+    setData({ ...data, [e.target.name]: e.target.value });
+  }
+  const handleOnCancel = (e)=>{
+    e.preventDefault();
+    setData(dataCopy);
+    setChangeDetected(false)
+  }
   return (
     <>
       <div className="container mx-auto p-4">
@@ -16,12 +34,15 @@ const Profile = () => {
         </div>
         <hr className="mt-2" />
         <div className="w-full grid gap-2 grid-cols-6 mt-4">
-          <Sidebar/>
+          <Sidebar />
           <div className="md:col-span-5 col-span-6">
-            <div className="flex justify-end gap-4">
-              <button className="font-semibold text-sm border rounded-full aspect-square shrink-0 p-1 "><MdCancel /> </button>
-              <button className="font-semibold text-sm border rounded-full aspect-square shrink-0 p-1 bg-[#46a999] text-white"><MdCheck /> </button>
-            </div>
+            {
+              changeDetected &&
+              <div className="flex justify-end gap-4">
+                <button onClick={handleOnCancel} className="font-semibold text-sm border rounded-full aspect-square shrink-0 p-1 "><MdCancel /> </button>
+                <button className="font-semibold text-sm border rounded-full aspect-square shrink-0 p-1 bg-[#46a999] text-white"><MdCheck /> </button>
+              </div>
+            }
             <div className="w-full flex items-center px-4 mt-4">
               <div className="flex md:justify-betwen md:flex-row flex-col justify-center w-full gap-3 items-center z-10">
                 <div className="relative rounded-full">
@@ -30,8 +51,8 @@ const Profile = () => {
                 </div>
                 <div className="w-full flex md:gap-0 gap-4 md:flex-row flex-col justify-center items-center  md:justify-between">
                   <div>
-                    <h2 className="font-bold md:text-left text-center">Mohd Usman</h2>
-                    <h3 className="text-sm text-gray-500 md:text-left text-center">@mohd.usman_7d3</h3>
+                    <h2 className="font-bold md:text-left text-center">{data.name}</h2>
+                    <h3 className="text-sm text-gray-500 md:text-left text-center">@{data.username}</h3>
                   </div>
                 </div>
               </div>
@@ -41,17 +62,25 @@ const Profile = () => {
               <div className="col-span-3">
                 <div className="flex items-center border p-1 rounded">
                   <span className="border-r pr-1">duati.com/</span>
-                  <input type="text" className="border-none focus:outline-none pl-1 w-full" />
+                  <input type="text" value={data.username} onInput={(e) => { e.target.value = e.target.value.replace(/[^a-zA-Z0-9-_\.]/g, ""); }} onChange={handleOnChange} name="username" className="border-none focus:outline-none pl-1 w-full" />
                 </div>
               </div>
             </div>
             <hr className="my-2" />
-            <div className="w-full items-center grid grid-cols-5 p-4 text-sm">
-              <div className="col-span-2 font-semibold text-gray-500">Website</div>
+            <div className="w-full items-start grid grid-cols-5 p-4 text-sm">
+              <div className="col-span-2 font-semibold text-gray-500">Name</div>
               <div className="col-span-3">
                 <div className="flex items-center border p-1 rounded">
-                  <span className="border-r pr-1">https://</span>
-                  <input type="text" className="border-none focus:outline-none pl-1 w-full" />
+                  <input type="text" value={data.name}  onChange={handleOnChange} name="name" className="border-none focus:outline-none pl-1 w-full" />
+                </div>
+              </div>
+            </div>
+            <hr className="my-2" />
+            <div className="w-full items-start grid grid-cols-5 p-4 text-sm">
+              <div className="col-span-2 font-semibold text-gray-500">Bio</div>
+              <div className="col-span-3">
+                <div className="flex items-center border p-1 rounded">
+                  <textarea name="bio" id="bio" value={data.bio} onChange={handleOnChange} className="border-none focus:outline-none pl-1 w-full" cols="30" rows="4"></textarea>
                 </div>
               </div>
             </div>
@@ -60,7 +89,7 @@ const Profile = () => {
               <div className="col-span-2 font-semibold text-gray-500">Job Title</div>
               <div className="col-span-3">
                 <div className="flex items-center border p-1 rounded">
-                  <input type="text" className="border-none focus:outline-none pl-1 w-full" />
+                  <input type="text" value={data.title} onChange={handleOnChange} name="title" className="border-none focus:outline-none pl-1 w-full" />
                 </div>
               </div>
             </div>
@@ -69,7 +98,7 @@ const Profile = () => {
               <div className="col-span-2 font-semibold text-gray-500">Course</div>
               <div className="col-span-3">
                 <div className="flex items-center border p-1 rounded">
-                  <input type="text" className="border-none focus:outline-none pl-1 w-full" />
+                  <input type="text" value={data.course} onChange={handleOnChange} name="course" className="border-none focus:outline-none pl-1 w-full" />
                 </div>
               </div>
             </div>
@@ -77,17 +106,45 @@ const Profile = () => {
               <div className="col-span-2 font-semibold text-gray-500">Duration</div>
               <div className="col-span-3">
                 <div className="flex items-center justify-start">
-                  <select name="start" id="start" className="p-1 border focus:outline-none rounded">
+                  <select name="start" id="start" onChange={handleOnChange} value={data.start} className="p-1 border focus:outline-none rounded">
                     <option value="2020">2020</option>
-                    <option value="2020">2021</option>
-                    <option value="2020">2022</option>
+                    <option value="2021">2021</option>
+                    <option value="2022">2022</option>
+                    <option value="2023">2023</option>
+                    <option value="2024">2024</option>
                   </select>
                   <span className="mx-2">to</span>
-                  <select name="end" id="end" className="p-1 border focus:outline-none rounded">
-                    <option value="2020">2021</option>
-                    <option value="2020">2022</option>
-                    <option value="2020">2023</option>
+                  <select name="end" id="end" value={data.end} onChange={handleOnChange} className="p-1 border focus:outline-none rounded">
+                    <option value="2020">2020</option>
+                    <option value="2021">2021</option>
+                    <option value="2022">2022</option>
+                    <option value="2023">2023</option>
+                    <option value="2024">2024</option>
                   </select>
+                </div>
+              </div>
+            </div>
+            <hr className="my-2" />
+            <div className="w-full items-start grid grid-cols-5 p-4 text-sm">
+              <div className="col-span-2 font-semibold text-gray-500">Links</div>
+              <div className="col-span-3 space-y-4">
+                <div className="">
+                  <div className="flex items-center border p-1 rounded">
+                    <span className="border-r pr-1"><MdFacebook /></span>
+                    <input type="text" className="border-none focus:outline-none pl-1 w-full" />
+                  </div>
+                </div>
+                <div>
+                  <div className="flex items-center border p-1 rounded">
+                    <span className="border-r pr-1"><AiOutlineTwitter /> </span>
+                    <input type="text" className="border-none focus:outline-none pl-1 w-full" />
+                  </div>
+                </div>
+                <div>
+                  <div className="flex items-center border p-1 rounded">
+                    <span className="border-r pr-1"><MdWhatsapp /> </span>
+                    <input type="text" className="border-none focus:outline-none pl-1 w-full" />
+                  </div>
                 </div>
               </div>
             </div>
